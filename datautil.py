f19b62c1c8056e4578ecd030a3551426ea68292e
David Read <david.read@okfn.org>
2011-12-13 12:23:12 +0000

    datautil-date renamed to datautildate.

--- datautil.py
+++ datautil.py
@@ -3,14 +3,14 @@
 from builder_ckanext import builder as b
 
 b.run('Remove potential conflicts',
-      'rm -rf %(pyenv_dir)s/src/datautil-date')
+      'rm -rf %(pyenv_dir)s/src/datautildate')
 
-b.run('Install datautil-date',
-      'pip -E %(pyenv_dir)s install -e hg+https://okfn@bitbucket.org/okfn/datautil-date#egg=datautil-date')
+b.run('Install datautildate',
+      'pip -E %(pyenv_dir)s install -e hg+https://okfn@bitbucket.org/okfn/datautildate#egg=datautildate')
 
-b.run('Install datautil-date test deps manually',
+b.run('Install datautildate test deps manually',
       'pip -E %(pyenv_dir)s install nose xlrd gdata')
 
 b.run('Tests',
-      '. %(pyenv_dir)s/bin/activate; %(pyenv_dir)s/bin/nosetests -v %(src_dir)s/datautil-date/datautil/tests')
+      '. %(pyenv_dir)s/bin/activate; %(pyenv_dir)s/bin/nosetests -v %(src_dir)s/datautildate/datautildate/tests')
 
